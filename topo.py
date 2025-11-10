2fd78cd7504b198ec2956e6daa021436ed69ba46
jimmyto-09 <jimmy.acastilloa@gmail.com>
2025-11-10 16:39:55 +0100

    se añadio las MAC

--- topo.py
+++ topo.py
@@ -2,8 +2,8 @@ from mininet.topo import Topo
 
 class SimpleTopo(Topo):
     def build(self):
-        h1 = self.addHost('h1')
-        h2 = self.addHost('h2')
+        h1 = self.addHost('h1', ip='10.0.0.1/8', mac='00:00:00:00:00:01')
+        h2 = self.addHost('h2', ip='10.0.0.2/8', mac='00:00:00:00:00:02')
         s1 = self.addSwitch('s1')  # switch P4/BMv2
 
         self.addLink(h1, s1)
